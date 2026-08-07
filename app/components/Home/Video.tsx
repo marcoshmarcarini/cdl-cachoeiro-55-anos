e83f90ceb263f5f75ecfcc81461c5bd7e42bdc43
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2026-08-07 17:23:25 -0300

    Update Video.tsx

--- app/components/Home/Video.tsx
+++ app/components/Home/Video.tsx
@@ -16,19 +16,6 @@ export default function Video() {
         data-aos="fade-up"
         data-aos-delay="300"
       ></iframe>
-      <iframe
-        width="896"
-        height="500"
-        src="https://www.youtube.com/embed/w7-nas9efws?si=XpphlrqREfLPwhGP"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-        className="video-placeholder-container relative aspect-video w-full max-w-4xl rounded-2xl overflow-hidden group cursor-pointer transition-all hover:scale-[1.01] hover:border-secondary-brand/60"
-        data-aos="fade-up"
-        data-aos-delay="300"
-      ></iframe>
     </div>
   );
 }
